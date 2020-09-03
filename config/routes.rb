aedc2f334e10114fe8bf03ce5eeff56cc963fd06
royFloresNyc <royfloresnyc@gmail.com>
2020-09-03 13:52:05 -0400

    Complete Lab

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'users#home'
+  get '/login', to: 'sessions#new', as: 'login'
+  post '/login', to: 'sessions#create'
 end
