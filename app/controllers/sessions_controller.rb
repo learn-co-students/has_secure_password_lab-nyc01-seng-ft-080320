89681530fbf90b5df262b15abaaee18be39e6980
rachaelghorbani <rachael.ghorbani@gmail.com>
2020-09-03 13:03:53 -0400

    Done.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,10 +6,13 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(name: params[:user][:name])
     authenticated = user.try(:authenticate, params[:user][:password])
-    return redirect_to new_login_path unless authenticated
-    session[:user_id] = user.id
-    @user = user
-    redirect_to '/'
+    if authenticated
+        session[:user_id] = user.id
+        @user = user
+        redirect_to '/'
+    else 
+        redirect_to new_login_path
+    end
   end
 
   def destroy
